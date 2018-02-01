48a0eae57b42883f1fe22dd1a705fe6b7af46d12
hilliverson <hilliverson@gmail>
2018-02-01 13:34:45 +0800

    第一次上傳

---
+++ ConsoleEnumHelper/ConsoleEnumHelper/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEnumHelper
+{
+    public enum LanguageEnum
+    {
+        [Description("en-US")]
+        English = 1,
+        [Description("ja-JP")]
+        Japanese = 2
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "en-US";
+            if (EnumHelper.TryGetValueFromDescription<LanguageEnum>("en-US")) 
+                Console.WriteLine(name);
+
+            Console.WriteLine(LanguageEnum.English.GetDescription());
+            Console.ReadLine();
+        }
+    }
+}
